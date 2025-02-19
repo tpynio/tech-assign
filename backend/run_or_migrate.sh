5e29ee73856b7c8754ac03b6c14df51fd09b54fd
Ivan <tpynyc@gmail.com>
2025-02-20 04:28:54 +0700

    Fix docker-compose: adding redis and celeries

--- backend/run_or_migrate.sh
+++ backend/run_or_migrate.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 if [ "$APPLY_MIGRATION" = 1 ] ; then
-     alembic upgrade head
+    alembic upgrade head
+elif [ "$CELERY_BEAT" = 1 ] ; then
+    celery -A periodic.celerybeat_schedule beat --loglevel=info
+elif [ "$CELERY_WORKER" = 1 ] ; then
+    celery -A periodic.tasks worker --loglevel=info
 else
-     python -m main
+    python -m main
 fi
